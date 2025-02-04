155b85195936fa7441095fefd6666547b9694d14
Mahesh Babu . Karna <karnamaheshbabu32@gmail.com>
2025-02-04 09:55:03 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
     <meta name="google-site-verification" content="YYLXpVnGVsxdSy2Nl-KVOHB8JM9grUzLQ__Qy55vH0o" />
-      <link rel="icon" type="image/jpeg" href="/itachieyes.jpg" />
+      <link rel="icon" type="image/svg" href="/logo.svg" />
       <body
         className= {`${geistSans.variable} ${geistMono.variable} antialiased font-sans relative`}
       >
