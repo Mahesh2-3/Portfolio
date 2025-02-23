ae6de9828e227dfb10dfa5c444622e74aca248aa
Mahesh Babu . Karna <karnamaheshbabu32@gmail.com>
2025-02-23 11:29:57 +0530

    Update page.js

--- app/myprojects/page.js
+++ app/myprojects/page.js
@@ -16,13 +16,24 @@ const page = () => {
             <img className='mx-auto hidden md:flex' src="./projects.png" alt="" />
           </div>
           <div>
-            <div className="border-b pb-[100px] w-[80%] mx-auto flex justify-evenly items-center gap-7 my-[100px] lg:flex-row flex-col">
-              <a target='__blank__' className='lg:w-[50%] w-[100%]' href="https://github.com/Mahesh2-3/Password-Manager">
-<video className="shadow-[0px_0px_17px_2px_rgba(255,255,255,0.8)] w-[100%] cursor-pointer " playsInline  autoPlay id="myVideo" loop>
-                  <source src="./password.mp4" type="video/mp4"/>
-                    Your browser does not support the video tag.
-                </video>        
-        </a>
+            <div className="border-b pb-[100px] w-[80%] mx-auto flex justify-evenly items-center gap-7 my-[100px] lg:flex-row flex-col-reverse">
+              <p className="leading-10 text-3xl text-[#FFD700] drop-shadow-[0px_0px_40px_rgba(255,255,255,1)] font-bold m-auto">AOT-Game</p>
+              <a target='__blank__' className='lg:w-[50%] w-[100%] ' href="https://mahesh2-3.github.io/AOT-Game/">
+                {/* <img className="shadow-[0px_0px_17px_2px_rgba(255,255,255,0.8)] w-[100%] cursor-pointer " src="./twit.png" alt="" /> */}
+                <video className="shadow-[0px_0px_17px_2px_rgba(255,255,255,0.8)] w-[100%] cursor-pointer " playsInline autoPlay id="myVideo" loop>
+                  <source src="aot.mp4" type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+              </a>
+            </div>
+            <div className="border-b pb-[100px] w-[80%] mx-auto flex justify-evenly items-center gap-7 mb-[100px] lg:flex-row flex-col">
+              <a target='__blank__' className='lg:w-[50%] w-[100%]' href="https://mahesh2-3.github.io/Password-Manager/">
+                {/* <img className="shadow-[0px_0px_17px_2px_rgba(255,255,255,0.8)] w-[100%] cursor-pointer " src="./pass.png" alt="" /> */}
+                <video className="shadow-[0px_0px_17px_2px_rgba(255,255,255,0.8)] w-[100%] cursor-pointer " playsInline autoPlay id="myVideo" loop>
+                  <source src="./password.mp4" type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+              </a>
               <p className="leading-10 text-3xl text-[#FFD700] drop-shadow-[0px_0px_40px_rgba(255,255,255,1)] font-bold m-auto">Password Manager</p>
             </div>
             <div className="border-b pb-[100px] w-[80%] mx-auto flex justify-evenly items-center gap-7 mb-[100px] lg:flex-row flex-col-reverse">
