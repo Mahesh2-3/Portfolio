3967780f6ff2d3ee01d8079dbbd3bf588be0c869
Mahesh Babu . Karna <karnamaheshbabu32@gmail.com>
2025-03-18 18:41:58 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -22,7 +22,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <link rel="icon" type="image/jpeg" href="/itachieyes.jpg" />
+      <link rel="icon" type="image/svg" href="/logo.svg" />
       <body
         className= {`${geistSans.variable} ${geistMono.variable} antialiased font-sans relative`}
       >
