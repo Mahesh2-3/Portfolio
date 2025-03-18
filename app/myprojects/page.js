c25ebaf955b2b2c855de7409a0f14cca635790e7
Mahesh Babu . Karna <karnamaheshbabu32@gmail.com>
2025-03-18 18:26:50 +0530

    Update page.js

--- app/myprojects/page.js
+++ app/myprojects/page.js
@@ -22,8 +22,8 @@ const page = () => {
               <p className="leading-10 text-3xl text-[#FFD700] drop-shadow-[0px_0px_40px_rgba(255,255,255,1)] font-bold m-auto">AOT-Game</p>
               <a target='__blank__' className='lg:w-[50%] w-[100%] ' href="https://mahesh2-3.github.io/AOT-Game/">
                 {/* <img className="shadow-[0px_0px_17px_2px_rgba(255,255,255,0.8)] w-[100%] cursor-pointer " src="./twit.png" alt="" /> */}
-                <video className="shadow-[0px_0px_17px_2px_rgba(255,255,255,0.8)] w-[100%] cursor-pointer " playsInline autoPlay id="myVideo" loop>
-                  <source src="aot.mp4" type="video/mp4" />
+                <video className="shadow-[0px_0px_17px_2px_rgba(255,255,255,0.8)] w-[100%] cursor-pointer " preload="metadata" playsInline autoPlay id="myVideo" loop>
+                  <source src="aot(1).mp4"  type="video/mp4" />
                   Your browser does not support the video tag.
                 </video>
               </a>
@@ -31,7 +31,7 @@ const page = () => {
             <div className="border-b pb-[100px] w-[80%] mx-auto flex justify-evenly items-center gap-7 mb-[100px] lg:flex-row flex-col">
               <a target='__blank__' className='lg:w-[50%] w-[100%]' href="https://mahesh2-3.github.io/Password-Manager/">
                 {/* <img className="shadow-[0px_0px_17px_2px_rgba(255,255,255,0.8)] w-[100%] cursor-pointer " src="./pass.png" alt="" /> */}
-                <video className="shadow-[0px_0px_17px_2px_rgba(255,255,255,0.8)] w-[100%] cursor-pointer " playsInline autoPlay id="myVideo" loop>
+                <video className="shadow-[0px_0px_17px_2px_rgba(255,255,255,0.8)] w-[100%] cursor-pointer " preload="metadata" playsInline autoPlay id="myVideo" loop>
                   <source src="./password.mp4" type="video/mp4" />
                   Your browser does not support the video tag.
                 </video>
@@ -42,7 +42,7 @@ const page = () => {
               <p className="leading-10 text-3xl text-[#FFD700] drop-shadow-[0px_0px_40px_rgba(255,255,255,1)] font-bold m-auto">Twitter Clone</p>
               <a target='__blank__' className='lg:w-[50%] w-[100%] ' href="https://mahesh2-3.github.io/Twitter-Clone/">
                 {/* <img className="shadow-[0px_0px_17px_2px_rgba(255,255,255,0.8)] w-[100%] cursor-pointer " src="./twit.png" alt="" /> */}
-                <video className="shadow-[0px_0px_17px_2px_rgba(255,255,255,0.8)] w-[100%] cursor-pointer " playsInline autoPlay id="myVideo" loop>
+                <video className="shadow-[0px_0px_17px_2px_rgba(255,255,255,0.8)] w-[100%] cursor-pointer " preload="metadata" playsInline autoPlay id="myVideo" loop>
                   <source src="twittter.mp4" type="video/mp4" />
                   Your browser does not support the video tag.
                 </video>
@@ -51,7 +51,7 @@ const page = () => {
             <div className="border-b pb-[100px] w-[80%] mx-auto flex justify-evenly items-center gap-7 mb-[100px] lg:flex-row flex-col">
               <a target='__blank__' className='lg:w-[50%] w-[100%] ' href="https://mahesh2-3.github.io/Spotify-Clone/">
                 {/* <img className="shadow-[0px_0px_17px_2px_rgba(255,255,255,0.8)] w-[100%] cursor-pointer " src="./spot.png" alt="" /> */}
-                <video className="shadow-[0px_0px_17px_2px_rgba(255,255,255,0.8)] w-[100%] cursor-pointer " playsInline autoPlay id="myVideo" loop>
+                <video className="shadow-[0px_0px_17px_2px_rgba(255,255,255,0.8)] w-[100%] cursor-pointer " preload="metadata" playsInline autoPlay id="myVideo" loop>
                   <source src="spotify.mp4" type="video/mp4" />
                   Your browser does not support the video tag.
                 </video>
@@ -63,7 +63,7 @@ const page = () => {
               <p className="leading-10 text-3xl text-[#FFD700] drop-shadow-[0px_0px_40px_rgba(255,255,255,1)] font-bold m-auto">To do List</p>
               <a target='__blank__' className='lg:w-[50%] w-[100%]] ' href="https://mahesh2-3.github.io/To-do-List/">
                 {/* <img className="shadow-[0px_0px_17px_2px_rgba(255,255,255,0.8)] w-[100%] cursor-pointer " src="./todo.png" alt="" /> */}
-                <video className="shadow-[0px_0px_17px_2px_rgba(255,255,255,0.8)] w-[100%] cursor-pointer " playsInline autoPlay id="myVideo" loop>
+                <video className="shadow-[0px_0px_17px_2px_rgba(255,255,255,0.8)] w-[100%] cursor-pointer " preload="metadata" playsInline autoPlay id="myVideo" loop>
                   <source src="todo.mp4" type="video/mp4" />
                   Your browser does not support the video tag.
                 </video>
@@ -72,7 +72,7 @@ const page = () => {
             <div className="border-b pb-[50px] w-[80%] mx-auto flex justify-evenly items-center gap-7 mb-[100px]  lg:flex-row flex-col">
               <a target='__blank__' className='lg:w-[50%] w-[100%]] ' href="https://mahesh2-3.github.io/Netflix-Clone/">
                 {/* <img className="shadow-[0px_0px_17px_2px_rgba(255,255,255,0.8)] w-[100%] cursor-pointer " src="./netflix.png" alt="" /> */}
-                <video className="shadow-[0px_0px_17px_2px_rgba(255,255,255,0.8)] w-[100%] cursor-pointer " playsInline autoPlay loop id="myVideo">
+                <video className="shadow-[0px_0px_17px_2px_rgba(255,255,255,0.8)] w-[100%] cursor-pointer " preload="metadata" playsInline autoPlay loop id="myVideo">
                   <source src="netflix.mp4" type="video/mp4" />
                   Your browser does not support the video tag.
                 </video>
