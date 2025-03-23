514a2a209635888ee524841cf93f5d68edb6749a
Mahesh Babu . Karna <karnamaheshbabu32@gmail.com>
2025-03-23 23:36:03 +0530

    Update page.js

--- app/myprojects/page.js
+++ app/myprojects/page.js
@@ -20,7 +20,7 @@ const page = () => {
           <div>
          <div className="border-b pb-[100px] w-[80%] mx-auto flex justify-evenly items-center gap-7 mb-[100px] lg:flex-row flex-col">
               <a target='__blank__' className='lg:w-[50%] w-[100%]' href="https://resumecraft-32.vercel.app/">
-                <img className="shadow-[0px_0px_17px_2px_rgba(255,255,255,0.8)] w-[100%] cursor-pointer " src="./passop.png" alt="" />
+                <img className="shadow-[0px_0px_17px_2px_rgba(255,255,255,0.8)] w-[100%] cursor-pointer " src="./resumecraft.png" alt="" />
               </a>
               <p className="leading-10 text-3xl text-[#FFD700] drop-shadow-[0px_0px_40px_rgba(255,255,255,1)] font-bold m-auto">ResumeCraft</p>
             </div>
